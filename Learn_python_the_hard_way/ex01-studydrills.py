8259eb4272c42dd9edf06b3069f4220d4a0cd5a2
Shreyas Rao <71974786+lindy-dev@users.noreply.github.com>
2022-02-19 22:33:24 +0530

    Added solns for chapt 1 and 2

---
+++ Learn_python_the_hard_way/ex01-studydrills.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# ex01 : A good first program 
+
+print("Hello world")
+print("Hello again")
+print("I like typing this")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+# print('I "said" do not touch this')
+print('This is another line!')
