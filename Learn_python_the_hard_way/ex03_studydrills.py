83636108739b3b33d072cdbf57377cfe10d24df5
Shreyas Rao <71974786+lindy-dev@users.noreply.github.com>
2022-03-04 22:27:08 +0530

    Added solns for exercise 3

---
+++ Learn_python_the_hard_way/ex03_studydrills.py
@@ -0,0 +1,21 @@
+print(50*2)
+print(1/500)
+print(4*3-1)
+print(3.14 *2 *200)
+print(1.0 /20)
+
+import decimal 
+print(decimal.Decimal(9876) + decimal.Decimal("54321.0123456"))
+
+# Fraction 
+import fractions
+print(fractions.Fraction(1,3))
+print(fractions.Fraction(4,6))
+print(3 * fractions.Fraction(1,3))
+
+
+# Complex 
+print(3-4j)
+print(3-4J)
+print(complex( 3,-4))
+print(3 + 1J * 3j) 
