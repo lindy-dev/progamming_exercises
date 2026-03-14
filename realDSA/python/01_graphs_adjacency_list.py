12e768649b54ec36c5de89625fad9663fdeffbd3
lindy-dev <shreyasrao2021@gmail.com>
2026-03-14 10:06:47 +0530

    Updated adjacency list implementation of graph from book

--- realDSA/python/01_graphs_adjacency_list.py
+++ realDSA/python/01_graphs_adjacency_list.py
@@ -1,39 +1,82 @@
-from collections import defaultdict
+# Adjacency list implementation 
+# takes into account edge weights 
+
+class Vertex: 
+    def __init__(self, key):
+        self.key = key
+        self.neighbours = {}
+
+    def get_neighbour(self, other):
+        return self.neighbours.get(other, None)
+    
+    def set_neighbour(self, other, weight = 0):
+        self.neighbours[other] = weight
+
+    def __str__(self):
+        return (
+            f"{self.key} connected to: " + 
+            f"{[x.key for x in self.neighbours]}"
+        )
+    def __repr__(self):
+        return f"Vertex ({self.key}) "
+    
+    def get_neighbours(self):
+        return self.neighbours.keys()
+    
+    def get_key(self):
+        return self.key 
+    
+
 
 class Graph:
     def __init__(self):
-        # Dictionary to store the graph: {node: [list of neighbours]}
-        self.graph = defaultdict(list)
+        self.vertices = {}
 
-    def add_edge(self, u, v):
-        """Adds a directed edge from u to v."""
-        self.graph[u].append[v]
+    def set_vertex(self, key):
+        self.vertices[key] = Vertex(key)
 
-    def add_undirected_edge(self, u, v):
-        """Adds an undirected edge (both directions)."""
-        self.add_edge(u, v)
-        self.add_edge(v, u)
-
-    def get_neighbours(self, vertex):
-        """Returns a list of neighbors for a specific vertex."""
-        return self.graph.get(vertex, [])
-
-    def print_graph(self):
-        """Prints the adjacency list representation."""
-        for node in self.graph:
-            print(f"{node} -> {self.graph[node]}")
-
-# --- Usage Example ---
-if __name__ == "__main__":
-    g = Graph()
+    def get_vertex(self, key):
+        return self.vertices.get(key, None)
     
-    # Create a graph: A->B, A->C, B->D
-    g.add_edge('A', 'B')
-    g.add_edge('A', 'C')
-    g.add_edge('B', 'D')
+    def __contains__(self, key):
+        return key in self.vertices
     
-    print("Graph Structure:")
-    g.print_graph()
+    def add_edge(self, from_vert, to_vert, weight=0):
+        if from_vert not in self.vertices:
+            self.set_vertex(from_vert)
 
-    # Check neighbors of 'A'
-    print(f"\nNeighbors of A: {g.get_neighbors('A')}")
+        if to_vert not in self.vertices:
+            self.set_vertex(to_vert)
+
+        self.vertices[from_vert].set_neighbour(
+            self.vertices[to_vert], weight
+        )
+
+    def get_vertices(self):
+        return self.vertices.keys()
+    
+    def __iter__(self):
+        return iter(self.vertices.values())
+
+
+# Example usage
+g = Graph()
+for i in range(6):
+    g.set_vertex(i)
+
+print(g.vertices)
+
+g.add_edge(0,1,5)
+g.add_edge(0,5,2)
+g.add_edge(1,2,4)
+g.add_edge(2,3,9)
+g.add_edge(3,4,7)
+g.add_edge(3,5,3)
+g.add_edge(4,0,1)
+g.add_edge(5,4,8)
+g.add_edge(5,2,1)
+
+for v in g:
+    for w in v.get_neighbours():
+        print(f"({v.get_key()}, {w.get_key()})")
+
